fab2eeb7fa11c2849627fd8ff4a3b13ae7583c0d
blancior <pkedziora347@gmail.com>
2024-03-17 22:16:53 +0100

    Style: Project site styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,35 @@
+import styled from "styled-components";
+
+const ProjectCard = styled.div`
+        width: 12rem;
+        height: 8rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 1vh;
+        border-radius: 25px;
+        background-color: ${({theme}) => theme.cardLight};
+        color: ${({theme}) => theme.textPrimary};
+        transition: 0.3s ease-in-out;
+        &:hover{
+                transform: translateY(-10%);
+                transition: 0.3s ease-in-out;
+        }
+`
+
+const ProjectsContainer = styled.div`
+`
+
 const Projects = () => {
     return(
         <>
-            <div>Project</div>
-            <div>Project</div>
-            <div>Project</div>
-            <div>Project</div>
+            <h1>Projects</h1>
+           <ProjectsContainer id='projects' className="grid grid-cols-2 grid-rows-2 gap-4">
+                   <ProjectCard >Project</ProjectCard>
+                   <ProjectCard >Project</ProjectCard>
+                   <ProjectCard >Project</ProjectCard>
+                   <ProjectCard >Project</ProjectCard>
+           </ProjectsContainer>
         </>
     )
 }
