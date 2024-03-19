7ee2aa9608f0565ee3847cd7bf27142ca7a4579d
blancior <pkedziora347@gmail.com>
2024-03-19 23:49:21 +0100

    Feat: added information about me & icon assets

---
+++ src/assets/data.js
@@ -0,0 +1,24 @@
+export const aboutMe = {
+    name: 'Przemysław',
+    roles: [
+        'Software Engineer',
+        'Frontend Developer',
+        'Student',
+        'Programmer',
+    ],
+    description:
+        "I have strong organizational skills and demonstrate responsibility, proactivity and resilience in my work. " +
+        "I have a great ability to adapt to new challenges. I like to analyze problems and evaluate different solutions. " +
+        "I believe that organization is very important in order to achieve personal goals in most efficient way.",
+    githubLink: "https://github.com/PrzemyslawKedziora",
+    linkedinLink: "https://www.linkedin.com/in/przemyslawkedziora/",
+    mailingLink: "mailto:pkedziora347@gmail.com"
+}
+export const education = [
+    {
+        img: "sygnet_pl.jpg",
+        university: "Lublin University of Technology",
+        degree: "Bachelor Degree, Software Engineering",
+        date: "Oct 2021 - at the moment",
+    },
+    ]
